81b0570ba0b83b3106db21c5023789c974ba0fe5
eldamaulya <eldaqotulmaulya16@gmail.com>
2022-09-14 18:44:23 +0700

    coba

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,22 @@
 <script>
-
+// export default {
+//     data() {
+//         return {
+//             name: "tweet",
+//             maxLimit: 10
+//         },
+//         methods : {
+//             cekInp(){
+//                 this.name = this.name.substr(0, this.maxLimit);
+//             }
+//         }
+//         computed : {
+//             minLimit(){
+//                 return this.maxLimit - this.name.length;
+//             }
+//         }
+//     }
+// }
 </script>
 
 <template>
@@ -12,18 +29,42 @@
         </div>
     </div>
  </div>
+ <section class="section">
+ <form class="form">
+    <textarea class="tweet" name="tweet" id="tweet" placeholder="Write Something" rows="4"
+    v-model="tweet" @input="cekInp" :class="{borderWar : !minLimit}">{{ minLimit }}/{{ maxLimit }}</textarea>
+    <div class="footer">
+        <p class="panjang">1/10</p>
+        <div class="button">
+            <button type="submit" class="button button-primary">Tweet</button>
+        </div>
+    </div>
+</form>
+</section>
 </template>
 
 <style>
 .navbar{
     height: 100%;
     width: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 0 1rem;
+    margin: auto;
     display: flex;
     align-items: center;
     justify-content: space-between;
     background-color: aquamarine;
+}
+
+.form {
+    width: 100%;
+    height: 100%;
+    width: 100%;
+    align-items: center;
+    margin: auto;
+    padding: 1rem;
+}
+
+.textarea {
+    width: 100%;
+    background-color: #ffffff0d;
 }
 </style>
